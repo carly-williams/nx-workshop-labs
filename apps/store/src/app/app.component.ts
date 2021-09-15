199f80ee1a9058d3fd6d27fa35bcdbcca048c2eb
carly-williams <carly.kurth@gmail.com>
2021-09-15 09:26:54 -0700

    finished through lab 13

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { formatRating } from '@bg-hoard/store/util-formatters';
 import { HttpClient } from '@angular/common/http';
 import { Game } from '@bg-hoard/util-interface';
+
 @Component({
   selector: 'bg-hoard-root',
   templateUrl: './app.component.html',
