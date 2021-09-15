55c5d0e1a86e7b8904b13d811fea85d410f6ca8a
carly-williams <carly.kurth@gmail.com>
2021-09-15 09:00:06 -0700

    half way through lab 13

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,10 +1,52 @@
+// import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+// import { libraryGenerator } from '@nrwl/workspace/generators';
+
+// export default async function (tree: Tree, schema: any) {
+//   await libraryGenerator(tree, { name: schema.name });
+//   await formatFiles(tree);
+//   return () => {
+//     installPackagesTask(tree);
+//   };
+// }
+
+// import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+// import { libraryGenerator } from '@nrwl/workspace/generators';
+
+// export default async function(host: Tree, schema: any) {
+//   await libraryGenerator(host, {
+//     name: `util-${schema.name}`,
+//   });
+//   await formatFiles(host);
+//   return () => {
+//     installPackagesTask(host);
+//   };
+// }
+
+// import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+// import { libraryGenerator } from '@nrwl/workspace/generators';
+
+// export default async function(host: Tree, schema: any) {
+//   await libraryGenerator(host, {
+//     name: `util-${schema.name}`,
+//     directory: schema.directory
+//   });
+//   await formatFiles(host);
+//   return () => {
+//     installPackagesTask(host);
+//   };
+// }
+
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
-  await formatFiles(tree);
+export default async function(host: Tree, schema: any) {
+  await libraryGenerator(host, {
+    name: `util-${schema.name}`,
+    directory: schema.directory,
+    tags: `type:util, scope:${schema.directory}`
+  });
+  await formatFiles(host);
   return () => {
-    installPackagesTask(tree);
+    installPackagesTask(host);
   };
 }
